67bea101ea32f58ed3325c69ad12849f87e8b358
Leonardo Ramos <leoramosdsousa@gmail.com>
2021-07-25 16:10:54 -0300

    added password hash

--- src/shared/container/index.ts
+++ src/shared/container/index.ts
@@ -1,9 +1,11 @@
 import { container } from "tsyringe";
 
-import { ICategoriesRepository } from "../../Modules/cars/repositories/ICategoriesRepository";
-import { CategoriesRepository } from "../../Modules/cars/repositories/implementations/CategoriesRepository";
-import { SpecificationsRepository } from "../../Modules/cars/repositories/implementations/SpecificationsRepository";
-import { ISpecificationsRepository } from "../../Modules/cars/repositories/ISpecificationsRepository";
+import { UsersRepository } from "../../modules/accounts/repositories/implementations/UsersRepository";
+import { IUsersRepository } from "../../modules/accounts/repositories/IUsersRepository";
+import { ICategoriesRepository } from "../../modules/cars/repositories/ICategoriesRepository";
+import { CategoriesRepository } from "../../modules/cars/repositories/implementations/CategoriesRepository";
+import { SpecificationsRepository } from "../../modules/cars/repositories/implementations/SpecificationsRepository";
+import { ISpecificationsRepository } from "../../modules/cars/repositories/ISpecificationsRepository";
 
 container.registerSingleton<ICategoriesRepository>(
     "CategoriesRepository",
@@ -13,4 +15,9 @@ container.registerSingleton<ICategoriesRepository>(
 container.registerSingleton<ISpecificationsRepository>(
     "SpecificationsRepository",
     SpecificationsRepository
+);
+
+container.registerSingleton<IUsersRepository>(
+    "UsersRepository",
+    UsersRepository
 );
