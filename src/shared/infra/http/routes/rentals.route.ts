3e8e65d208c27e12d2f9fe8319b22d2e19ac1d99
Leonardo Ramos <leoramosdsousa@gmail.com>
2021-08-12 10:20:11 -0300

    added create rental and unitary test create rental with dayjs lib

---
+++ src/shared/infra/http/routes/rentals.route.ts
@@ -0,0 +1,13 @@
+import { Router } from "express";
+
+import { CreateRentalController } from "@modules/rentals/useCases/createRental/CreateRentalController";
+
+import { ensureAuthenticated } from "../middleware/ensureAuthenticated";
+
+const rentalsRoutes = Router();
+
+const createRentalController = new CreateRentalController();
+
+rentalsRoutes.post("/", ensureAuthenticated, createRentalController.handle);
+
+export { rentalsRoutes };
